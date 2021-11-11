d90b085c3da0df76901f9db312c810746e3bee9e
fstrateg <Alexeym78@mail.ru>
2021-11-11 23:23:21 +0600

    First step of identification user

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,46 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const express_session_1 = __importDefault(require("express-session"));
+const path_1 = __importDefault(require("path"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
+app.use((0, express_session_1.default)({
+    secret: 'secret2142citten',
+    resave: true,
+    saveUninitialized: true
+}));
 app.get('/', (req, res) => {
-    res.send('Well done!');
+    if (req.session.Loggedin) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write('Welcome, ' + req.session.Username);
+        res.write('<br><a href="/logout">Logout</a>');
+        res.end();
+    }
+    else
+        res.sendFile(path_1.default.join(__dirname + '/view/login.html'));
+});
+app.post('/auth', (req, res) => {
+    console.log(req.body);
+    let username = req.body.username;
+    let pass = req.body.password;
+    if (username && pass) {
+        let Session = req.session;
+        Session.Loggedin = true;
+        Session.Username = username;
+        res.redirect('/');
+    }
+    else
+        res.send('Please enter username and password');
+    res.end();
+});
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        res.redirect('/');
+    });
 });
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,61 @@
-import express, { Request, Response, Application} from 'express';
+import express, { Request, Response, Application } from 'express'
+import session from 'express-session'
+import path from 'path'
+import bodyParser from 'body-parser'
 
-const app: Application = express();
-const port = 3000;
+declare module 'express-session' {
+    export interface Session {
+        Loggedin: boolean,
+        Username: string
+    }
+}
+
+const app: Application = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(session({
+    secret: 'secret2142citten',
+    resave: true,
+    saveUninitialized: true
+}));
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Well done!');
-});
+    if (req.session.Loggedin) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write('Welcome, ' + req.session.Username)
+        res.write('<br><a href="/logout">Logout</a>')
+        res.end()
+    }
+        
+    else
+        res.sendFile(path.join(__dirname + '/view/login.html'))
+
+})
+
+app.post('/auth', (req: Request, res: Response) => {
+    console.log(req.body)
+    let username: string = req.body.username
+    let pass: string = req.body.password
+    if (username && pass) {
+        let Session = req.session
+        Session.Loggedin = true
+        Session.Username = username
+        res.redirect('/')
+    }
+    else
+        res.send('Please enter username and password')
+    res.end()
+})
+
+app.get('/logout', (req: Request, res: Response) => {
+    req.session.destroy((err: any) => {
+        res.redirect('/')
+    })
+})
+
 app.listen(port, ():void => {
     return console.log(`server is listening on ${port}`);
 });
